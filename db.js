d4c000b2dd741cd98a4c3d58cb65a67ab643dda5
softdev1844 <soft.dev1844@gmail.com>
2025-02-09 15:46:34 +0530

    validate mongoURI before connecting

--- db.js
+++ db.js
@@ -1,14 +1,21 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 dotenv.config();
 
-const ConnectMongoDB = async()=>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.warn('MongoDB Database Connected Successfully');
-    } catch (error) {
-        console.warn('Error while connecting to MongoDB',error);
+const ConnectMongoDB = async () => {
+  try {
+    const mongoURI = process.env.MONGODB_URI; // Ensure this is set
+    if (!mongoURI) {
+      console.error(
+        "Error: MONGO_URI is not defined in environment variables."
+      );
+      process.exit(1); // Exit if no URI is provided
     }
-}
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.warn("MongoDB Database Connected Successfully");
+  } catch (error) {
+    console.warn("Error while connecting to MongoDB", error);
+  }
+};
 
-module.exports  = { ConnectMongoDB };
+module.exports = { ConnectMongoDB };
