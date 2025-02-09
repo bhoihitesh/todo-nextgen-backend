33fa6c253258e594477e18939f44d57f0d9fa775
softdev1844 <soft.dev1844@gmail.com>
2025-02-09 16:01:18 +0530

    changes

--- db.js
+++ db.js
@@ -4,13 +4,6 @@ dotenv.config();
 
 const ConnectMongoDB = async () => {
   try {
-    const mongoURI = process.env.MONGODB_URI; // Ensure this is set
-    if (!mongoURI) {
-      console.error(
-        "Error: MONGO_URI is not defined in environment variables."
-      );
-      process.exit(1); // Exit if no URI is provided
-    }
     await mongoose.connect("mongodb+srv://bhoihitesh1492:$*Hitesh%40123*$@todo-nextgen.ew82p.mongodb.net/todo-app?retryWrites=true&w=majority");
     console.warn("MongoDB Database Connected Successfully");
   } catch (error) {
