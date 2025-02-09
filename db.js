900473595c028b3a2794a5b832aa56a5c3fa9546
softdev1844 <soft.dev1844@gmail.com>
2025-02-09 15:59:52 +0530

    changes

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ const ConnectMongoDB = async () => {
       );
       process.exit(1); // Exit if no URI is provided
     }
-    await mongoose.connect(mongoURI);
+    await mongoose.connect("mongodb+srv://bhoihitesh1492:$*Hitesh%40123*$@todo-nextgen.ew82p.mongodb.net/todo-app?retryWrites=true&w=majority");
     console.warn("MongoDB Database Connected Successfully");
   } catch (error) {
     console.warn("Error while connecting to MongoDB", error);
