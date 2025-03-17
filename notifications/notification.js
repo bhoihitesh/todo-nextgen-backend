b0543519e8f55baa3209728676f7dda57d64968d
softdev1844 <soft.dev1844@gmail.com>
2025-03-18 04:04:20 +0530

    re-enable the firebase notifications feature

--- notifications/notification.js
+++ notifications/notification.js
@@ -1,35 +1,35 @@
-// const admin = require('firebase-admin');
-// const dotenv = require('dotenv');
-// dotenv.config();
+const admin = require('firebase-admin');
+const dotenv = require('dotenv');
+dotenv.config();
 
-// // Decode and parse the Base64-encoded credentials
-// const serviceAccount = JSON.parse(
-//   Buffer.from(process.env.GOOGLE_CLOUD_CREDENTIALS_BASE64, "base64").toString("utf-8")
-// );
+// Decode and parse the Base64-encoded credentials
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.GOOGLE_CLOUD_CREDENTIALS_BASE64, "base64").toString("utf-8")
+);
 
-// // Initialize Firebase Admin SDK
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
+// Initialize Firebase Admin SDK
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
-// function sendNotification(fcmToken, title, body) {
-//     const message = {
-//       notification: {
-//         title: title,
-//         body: body
-//       },
-//       token: fcmToken
-//     };
+function sendNotification(fcmToken, title, body) {
+    const message = {
+      notification: {
+        title: title,
+        body: body
+      },
+      token: fcmToken
+    };
 
-//     // Send the notification
-//     admin.messaging().send(message)
-//       .then((response) => {
-//         console.log('Successfully sent message:', response);
-//       })
-//       .catch((error) => {
-//         console.error('Error sending message:', error);
-//       });
-//   }
+    // Send the notification
+    admin.messaging().send(message)
+      .then((response) => {
+        console.log('Successfully sent message:', response);
+      })
+      .catch((error) => {
+        console.error('Error sending message:', error);
+      });
+  }
 
-//   module.exports = sendNotification;
+  module.exports = sendNotification;
   
