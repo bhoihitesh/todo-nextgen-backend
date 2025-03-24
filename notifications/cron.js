5ffb51ddedfd3526778fdd42ec0a0b1e79e75b86
softdev1844 <soft.dev1844@gmail.com>
2025-03-25 00:32:06 +0530

    cron job changes

--- notifications/cron.js
+++ notifications/cron.js
@@ -7,19 +7,19 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const notifyMe =()=> {
-    return setInterval(async() => {
-        const userData = await users.find();
-        const recordData = await records.find()
+    // return setInterval(async() => {
+    //     const userData = await users.find();
+    //     const recordData = await records.find()
         
-        userData.forEach((user,i)=>{
-            recordData.forEach((record,i)=>{
-                const recordDateTime = record?.record_start_date + ' ' + record?.record_start_time;
-                const givenDateTime = moment(recordDateTime ,"DD-MM-YYYY hh:mm A");
-                const currentDateTime = moment();
-                if(currentDateTime.isSameOrAfter(givenDateTime) && user?.fcm_token) sendNotification(user?.fcm_token,record?.title, record?.description);
-            })
-        });
-    }, 10000);
+    //     userData.forEach((user,i)=>{
+    //         recordData.forEach((record,i)=>{
+    //             const recordDateTime = record?.record_start_date + ' ' + record?.record_start_time;
+    //             const givenDateTime = moment(recordDateTime ,"DD-MM-YYYY hh:mm A");
+    //             const currentDateTime = moment();
+    //             if(currentDateTime.isSameOrAfter(givenDateTime) && user?.fcm_token) sendNotification(user?.fcm_token,record?.title, record?.description);
+    //         })
+    //     });
+    // }, 10000);
 }
 
 module.exports = notifyMe;
