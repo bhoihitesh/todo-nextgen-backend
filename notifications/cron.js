b0a50b40693339b8cb8d366cb7b31c9f249f0b94
softdev1844 <soft.dev1844@gmail.com>
2025-03-29 19:23:52 +0530

    Added time zone for render.com

--- notifications/cron.js
+++ notifications/cron.js
@@ -2,7 +2,7 @@ const { default: axios } = require("axios");
 const users = require("../models/users");
 const records = require("../models/records");
 const sendNotification = require("./notification");
-const moment = require("moment");
+const moment = require("moment-timezone");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -10,21 +10,14 @@ const notifyMe = () => {
   return setInterval(async () => {
     const userData = await users.find();
     const recordData = await records.find();
-    const currentDateTime = moment(new Date(), "DD-MM-YYYY hh:mm A");
+    const currentDateTime = moment().tz("Asia/Kolkata");
     console.log("currentDateTime", currentDateTime)
 
     userData.forEach((user, i) => {
       recordData.forEach((record, i) => {
-        const recordDateTime =
-          record?.record_start_date + " " + record?.record_start_time;
-        const recordStartDateTime = moment(
-          recordDateTime,
-          "DD-MM-YYYY hh:mm A"
-        );
-        const recordEndDateTime = moment(
-          recordDateTime,
-          "DD-MM-YYYY hh:mm A"
-        ).add(1, "hours");
+        const recordDateTime = record?.record_start_date + " " + record?.record_start_time;
+        const recordStartDateTime = moment(recordDateTime, "DD-MM-YYYY hh:mm A");
+        const recordEndDateTime = moment(recordDateTime, "DD-MM-YYYY hh:mm A").add(1, "hours");
         console.log("condition1", recordStartDateTime, recordEndDateTime);
         console.log(
           "condition2",
@@ -43,7 +36,7 @@ const notifyMe = () => {
         }
       });
     });
-  }, 600000);
+  }, 10000);
 };
 
 module.exports = notifyMe;
